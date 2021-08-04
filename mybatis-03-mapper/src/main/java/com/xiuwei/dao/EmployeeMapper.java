d620aeb3a2a336f7b2b049f1d3443058c6df17f7
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2021-08-04 14:45:04 +0800

    crud

--- mybatis-03-mapper/src/main/java/com/xiuwei/dao/EmployeeMapper.java
+++ mybatis-03-mapper/src/main/java/com/xiuwei/dao/EmployeeMapper.java
@@ -6,11 +6,11 @@ public interface EmployeeMapper {
 
     /**
      *  可以为 增删改 定义以下返回值（无需修改mapper文件，不用加resultType，也没有resultType这个标签）
-     *  Boolean(boolean): 增删改是否成功
-     *  Long(long)/Integer(int): 增删改影响的行数
+     *      Boolean(boolean): 增删改是否成功。如果影响的行数>=1，即为true。
+     *      Long(long)/Integer(int): 增删改影响的行数
      */
     Employee getEmpById(Integer id);
     Long addEmp(Employee employee);
     Boolean updateEmp(Employee employee);
-    Integer  deleteEmp(Integer id);
+    Integer deleteEmp(Integer id);
 }
