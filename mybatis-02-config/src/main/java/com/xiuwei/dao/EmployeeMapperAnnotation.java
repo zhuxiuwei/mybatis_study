c0ebaa27d4276a1572fb34b5b6a1182ed93c6661
zhuxiuwei <zhuxiuwei@zhuxiuweideMacBook-Pro.local>
2021-08-04 10:24:15 +0800

    配置wenj

---
+++ mybatis-02-config/src/main/java/com/xiuwei/dao/EmployeeMapperAnnotation.java
@@ -0,0 +1,10 @@
+package com.xiuwei.dao;
+
+import com.xiuwei.POJO.Employee;
+import org.apache.ibatis.annotations.Select;
+
+public interface EmployeeMapperAnnotation {
+
+    @Select("select * from tbl_employee where id=#{id}")
+    public Employee getEmpById(Integer id);
+}
