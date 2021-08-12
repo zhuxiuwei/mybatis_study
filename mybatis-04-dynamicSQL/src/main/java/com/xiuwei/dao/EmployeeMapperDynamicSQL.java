bf5eff200f020d649d39ed6d8de3f1460785876d
zhuxiuwei <jlu_zxw@126.com>
2021-08-13 00:26:51 +0800

    chao40 where

--- mybatis-04-dynamicSQL/src/main/java/com/xiuwei/dao/EmployeeMapperDynamicSQL.java
+++ mybatis-04-dynamicSQL/src/main/java/com/xiuwei/dao/EmployeeMapperDynamicSQL.java
@@ -5,6 +5,9 @@ import com.xiuwei.POJO.Employee;
 import java.util.List;
 
 public interface EmployeeMapperDynamicSQL {
-    //查询条件employee里，携带了哪个查询条件，返回结果就带上这个字段的值
+    //#39： 查询条件employee里，携带了哪个查询条件，返回结果就带上这个字段的值
     List<Employee>  getEmpsByConditionIf(Employee employee);
+
+    //#40： 用if的同时，用where标签，解决where 后面and/or拼接可能存在的问题。
+    List<Employee>  getEmpsByConditionIfUsingWhere(Employee employee);
 }
--- mybatis-04-dynamicSQL/src/test/java/MyTest.java
+++ mybatis-04-dynamicSQL/src/test/java/MyTest.java
@@ -20,6 +20,7 @@ public class MyTest {
     }
 
     @Test
+    //#39： 查询条件employee里，携带了哪个查询条件，返回结果就带上这个字段的值
     public void testGetEmpsByConditionIf() throws IOException {
         SqlSession session = getSession();
         try {
@@ -30,6 +31,23 @@ public class MyTest {
              * 观察Log里真实执行的SQL：select * from tbl_employee where id=? and last_name like ?
              * 因为email和gender都是null, sql里就没包含这些字段。
             */
+        }finally {
+            session.close();
+        }
+    }
+
+    @Test
+    //#40： 用if的同时，用where标签，解决where 后面and/or拼接可能存在的问题。
+    public void testGetEmpsByConditionIfUsingWhere() throws IOException {
+        SqlSession session = getSession();
+        try {
+            EmployeeMapperDynamicSQL mapper = session.getMapper(EmployeeMapperDynamicSQL.class);
+            List<Employee> employees = mapper.getEmpsByConditionIfUsingWhere(new Employee(null, "%to%", null, null));
+            System.out.println(employees);  //[Employee{id=1, lastName='tom', gender='0', email='tom@126.com', department=null}]
+            /**
+             * 观察Log里真实执行的SQL，是语法正确的：select * from tbl_employee WHERE last_name like ?
+             * 即使第一个参数(id)为空，也没有错误拼接成 select * from tbl_employee WHERE and last_name like ?
+             */
         }finally {
             session.close();
         }
