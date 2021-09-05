af1e531a198070357598f9cb097077efecfddb14
zhuxiuwei <jlu_zxw@126.com>
2021-09-05 23:56:32 +0800

    69 gen code

---
+++ mybatis-07-mbg/src/test/java/GenerateCodeUsingJavaTest.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateCodeUsingJavaTest {
+
+    //#69: 测试mbg自动生成mybatis代码和mapper配置
+    @Test
+    public void testMbg() throws Exception{
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        File configFile = new File("mbg.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+}
